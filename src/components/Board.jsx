b2acc0c6229e119ca73519b0306d11a6ce37144c
anilk1995 <128927343+anilk1995@users.noreply.github.com>
2024-01-21 17:01:17 +0530

    introduced a playere to the game

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,15 +3,21 @@ import Square from './Square';
 
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
 
   const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition]) return;
+
     setSquares(currentSquares => {
       return currentSquares.map((squareValue, squareIndex) => {
-        if (clickedPosition === squareIndex) return 'X';
+        if (clickedPosition === squareIndex) {
+          return isXNext ? 'X' : 'O';
+        }
 
         return squareValue;
       });
     });
+    setIsXNext(prev => !prev);
   };
 
   const renderSquare = position => {
